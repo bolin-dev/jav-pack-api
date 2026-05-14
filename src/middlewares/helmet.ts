760ba8a1e44dc169dea078f4bd99d833ab255978
bolinc <bolinchan2046@gmail.com>
2026-05-14 16:00:59 +0800

    feat: 修正 helmet 中间件以使用正确的响应头设置方法

--- src/middlewares/helmet.ts
+++ src/middlewares/helmet.ts
@@ -3,6 +3,6 @@ import { createMiddleware } from "hono/factory";
 export const helmet = () => {
   return createMiddleware(async (c, next) => {
     await next();
-    c.header("X-Content-Type-Options", "nosniff");
+    c.res.headers.set("X-Content-Type-Options", "nosniff");
   });
 };
